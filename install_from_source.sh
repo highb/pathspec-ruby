fa3ed3df791ce729ed9cd3f05e9f5f454b7dad5e
Brandon High <brandon.high@puppetlabs.com>
2014-08-04 15:14:49 -0700

    readme and easy install script

---
+++ install_from_source.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+bundle install
+
+# Ensure this build is sane
+bundle exec rspec spec
+
+# Ensure there is no existing version
+bundle exec gem uninstall pathspec
+
+# Build and install!
+bundle exec gem build pathspec.gemspec && bundle exec gem install pathspec-0.0.1.gem
